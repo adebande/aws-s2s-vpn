70144170c3112823973f91f56104b457ce46234d
Aloïs Debande <alois.debande@cgi.com>
2022-06-07 10:39:56 +0200

    add s3 backend with default encryption

--- providers.tf
+++ providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.17.1"
     }
   }
+  backend "s3" {
+    bucket  = "s2s-terraform-backend"
+    key     = "terraform.tfstate"
+    region  = "eu-west-3"
+    encrypt = true
+  }
 }
 
 provider "aws" {
